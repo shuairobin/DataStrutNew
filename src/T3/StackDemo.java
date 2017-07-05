8cded4436c5f94fdf2f5a6483d879fadcd08e79e
Robin <jyfsx@126.com>
2017-07-05 20:04:04 +0800

    stack

--- src/T3/StackDemo.java
+++ src/T3/StackDemo.java
@@ -1,7 +1,22 @@
 package T3;
 
+import java.util.Stack;
+
 /**
  * Created by Robin on 2017/7/5.
  */
 public class StackDemo {
+    public static void main(String[] args){
+        Stack<String> stack = new Stack<>();
+        stack.push("组长");
+        stack.push("部门经理");
+        stack.push("主管副总");
+        stack.push("总经理");
+        stack.push("董事长");
+        System.out.println(stack.peek());
+        System.out.println(stack.peek());
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+    }
 }
