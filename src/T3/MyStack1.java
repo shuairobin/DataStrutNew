b55c4815f8f3979ac444636d98b5253b7f16c815
Robin <jyfsx@126.com>
2017-07-05 20:31:12 +0800

    getMin

--- src/T3/MyStack1.java
+++ src/T3/MyStack1.java
@@ -1,7 +1,44 @@
 package T3;
 
+import java.util.Stack;
+
 /**
  * Created by Robin on 2017/7/5.
  */
 public class MyStack1 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MyStack1(){
+        this.stackData = new Stack<>();
+        this.stackMin = new Stack<>();
+    }
+    public void push(int newNum){
+        if(this.stackMin.isEmpty()){
+            this.stackMin.push(newNum);
+        }else if(newNum <= this.getmin() ){
+            this.stackMin.push(newNum);
+        }
+        this.stackData.push(newNum);
+
+    }
+
+    public int pop(){
+        if(this.stackData.isEmpty()){
+            throw new RuntimeException("Your stack is empty");
+        }
+        int value = this.stackData.pop();
+        if(value == this.getmin()){
+            this.stackMin.pop();
+        }
+        return value;
+    }
+
+    public int getmin(){
+        if(this.stackMin.isEmpty()){
+            throw new RuntimeException("Your stack is empty.");
+
+        }
+        return this.stackMin.peek();
+    }
 }
